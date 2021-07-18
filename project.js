449bba3f0e073fbbf1b52313e3a0ca102653d622
glace23 <vikash23galce@gmail.com>
2021-07-17 20:19:20 -0700

    add lookup page and links

--- project.js
+++ project.js
@@ -19,6 +19,10 @@ app.get('/',function(req,res){
   res.render('home')
 });
 
+app.get('/lookup',function(req,res){
+  res.render('lookup')
+});
+
 app.get('/add',function(req,res){
   res.render('add')
 });

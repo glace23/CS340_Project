bc23b525f6c388abf6b6bfcfd5aafedf08491b6d
Josh Nguyen <Joshpnguyen@gmail.com>
2021-08-01 17:27:17 -0700

    Added insert function for students

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var mysql = require('./database/dbcon.js');
+var mysql = require('./dbcon.js');
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 PORT = 30245;
@@ -14,21 +14,8 @@ app.set('mysql', mysql);
 // post
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-app.get('/student', function (req, res){
-  context = {};
-  select_query = "SELECT studentFirstName, studentLastName, studentEmail, studentNumber, studentPhoneNumber FROM Students";
-  mysql.pool.query(select_query, (error, results, fields) => {
-    if(error) {
-        res.write(JSON.stringify(error));
-        res.end();
-    }
-    context.students = results;
-    return res.render('studentlookup', context);
-  });
-});
 
 app.get('/css', function(req,res){
       var options = { 
@@ -58,6 +45,43 @@ app.get('/room',function(req,res){
 });
 
 
+//====================== STUDENT SQL FUNCTIONS ======================
+app.get('/student', function (req, res){
+  context = {};
+  select_query = "SELECT studentFirstName, studentLastName, studentEmail, studentNumber, studentPhoneNumber, studentID FROM Students";
+  mysql.pool.query(select_query, (error, results, fields) => {
+    if(error) {
+        res.write(JSON.stringify(error));
+        res.end();
+    }
+    context.students = results;
+    return res.render('studentlookup', context);
+  });
+});
+
+app.post('/delete-student', function (req, res, next){
+  delete_query = "DELETE FROM Students WHERE studentID = ?;";
+  mysql.pool.query(delete_query, [req.body.id], function(err, result) {
+    if (err) {
+      next(err);
+      return
+    }
+    res.send();
+  })
+});
+
+app.post('/insert-student', function (req, res, next) {
+  insert_query = "INSERT INTO Students(studentFirstName, studentLastName, studentEmail, studentNumber, studentPhoneNumber) VALUES (?, ?, ?, ?, ?);";
+  mysql.pool.query(insert_query, 
+    [req.body.studentFirstName, req.body.studentLastName, req.body.studentEmail, req.body.studentNumber, req.body.studentPhoneNumber],
+    function(err, result) {
+      if (err) {
+        next(err);
+        return
+      }
+      res.send();
+    })
+});
 
 app.get('/add',function(req,res){
   res.render('add')
